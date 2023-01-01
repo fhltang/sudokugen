4bf8e0628310995bad26f39dbc7bc82f951bca73
Francis Tang <francis.tang@gmail.com>
2023-01-01 17:02:52 +0000

    Allow blanks count to passed in to CLI

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/fhltang/sudokugen/internal/generator"
 )
 
 func main() {
-	board, err := generator.Generate(50, 1)
+	blanks := 50
+
+	if len(os.Args) == 2 {
+		arg, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalf("Cannot parse blanks count %s: %v", os.Args[1], err)
+		}
+		blanks = arg
+	}
+
+	board, err := generator.Generate(blanks, 1)
 
 	if err != nil {
 		log.Fatalf("Failed to find board: %v", err)
